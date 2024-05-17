9462a0f225de8c307de69baeb4054716b094018c
asasakk <53592756+asasakk@users.noreply.github.com>
2024-05-17 15:10:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 
-dir = r"C:\Users\ko\Pictures\img"
+# ディレクトリ、検索文字列、置換文字列を指定
+directory = r"your Directory"
+search_string = r"変換前の文字列"
+replace_string = r"変化後の文字列"
+
 
 def replace_in_filenames(directory, search_string, replace_string):
     for filename in os.listdir(directory):
@@ -10,10 +14,7 @@ def replace_in_filenames(directory, search_string, replace_string):
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
             print(f"ファイル名を置換しました: {filename} -> {new_filename}")
 
-# ディレクトリ、検索文字列、置換文字列を指定
-directory = r"C:\Users\ko\Pictures\img"
-search_string = r"HEIC"
-replace_string = r"jpg"
+
 
 # 関数を呼び出して置換を実行
 replace_in_filenames(directory, search_string, replace_string)
